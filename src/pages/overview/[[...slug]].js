ac6d8c7ec02f4e34380f1ad9c35096d4760e9c88
ripperi <22086553+ripperi@users.noreply.github.com>
2023-09-12 19:48:34 +0300

    feat: add overview page

---
+++ src/pages/overview/[[...slug]].js
@@ -0,0 +1,51 @@
+import React from "react";
+import { useRouter } from "next/router";
+import { Container, Main } from "@urbit/fdn-design-system";
+import IntraNav from "../../components/IntraNav";
+import Footer from "../../components/Footer";
+import Content, {
+  getStaticProps as props,
+  getStaticPaths as paths,
+} from "../../components/Content";
+import contentTree from "../../../cache/overview.json";
+
+const root = "overview";
+
+export default function Overview({
+  search,
+  posts,
+  data,
+  markdown,
+  params,
+  previousPost,
+  nextPost,
+}) {
+  const router = useRouter();
+
+  return (
+    <Container>
+      <IntraNav />
+      <Main>
+        <Content
+          posts={posts}
+          data={data}
+          markdown={markdown}
+          params={params}
+          previousPost={previousPost}
+          nextPost={nextPost}
+          root={root}
+          path={router.asPath}
+        />
+      </Main>
+      <Footer />
+    </Container>
+  );
+}
+
+export async function getStaticProps({ params }) {
+  return props(params, root, contentTree);
+}
+
+export async function getStaticPaths() {
+  return paths(root, contentTree);
+}

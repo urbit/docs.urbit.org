6229a86a5cfb54892f8cfe02cd1e0a26fd64c4a5
ripperi <22086553+ripperi@users.noreply.github.com>
2023-09-14 15:05:43 +0300

    feat: add math jax startup scripts to document

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,4 +1,5 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
@@ -7,7 +8,44 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <Script
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `window.__MathJax_State__ = {
+          isReady: false,
+          promise: new Promise(resolve => {
+
+            window.MathJax = {
+              loader: {load: ['[tex]/autoload', '[tex]/ams']},
+              tex: {
+                packages: {'[+]': ['autoload', 'ams']},
+                processEscapes: true
+              },
+              jax: ["input/TeX","output/CommonHTML"],
+              options: {
+                renderActions: {
+                  addMenu: []
+                }
+              },
+              startup: {
+                typeset: false,
+                ready: () => {
+                  MathJax.startup.defaultReady();
+                  window.__MathJax_State__.isReady = true;
+                  resolve();
+                }
+              }
+            };
+          })
+        };`,
+          }}
+        />
+        <Script
+          strategy="beforeInteractive"
+          id="MathJax-script"
+          src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-chtml.js"
+        />
       </body>
     </Html>
-  )
+  );
 }

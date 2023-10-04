c7519bfd2c927f077fe9c06e64a5022d5b1da940
ripperi <22086553+ripperi@users.noreply.github.com>
2023-10-04 14:29:30 +0300

    feat: add meny tray for navigation on mobile

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 import { join } from "path";
 import classnames from "classnames";
@@ -33,6 +33,7 @@ function NavSection({
   path,
   indent = 0,
   divider = false,
+  longLabels = false,
 }) {
   const isUnderThisPage = `${path}/`.includes(`${root}/`);
   return (
@@ -46,7 +47,7 @@ function NavSection({
         style={{ paddingLeft: `${0.5 * indent}rem` }}
         href={`/${root}`}
       >
-        {label(posts.title)}
+        {longLabels ? posts.title : label(posts.title)}
       </Link>
       {isUnderThisPage &&
         posts.pages.map((page) => {
@@ -62,7 +63,7 @@ function NavSection({
               href={href}
               key={href}
             >
-              {label(page.title)}
+              {longLabels ? page.title : label(page.title)}
             </Link>
           );
         })}
@@ -77,6 +78,7 @@ function NavSection({
               path={path}
               indent={indent + 1}
               key={join(root, k)}
+              longLabels={longLabels}
             />
           );
         })}
@@ -94,8 +96,17 @@ export default function Content({
   root,
   path,
 }) {
+  const [isOpen, setIsOpen] = useState(false);
   const firstCrumb = path.split("#")[0];
   const md = JSON.parse(markdown);
+
+  if (typeof document !== "undefined") {
+    if (isOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "visible";
+    }
+  }
 
   const getInnerText = ({ children }) => {
     let innerText = "";
@@ -120,6 +131,75 @@ export default function Content({
 
   return (
     <div className="relative flex w-full">
+      {isOpen && (
+        <div className="flex md:hidden fixed z-50 left-0 top-0 w-screen h-screen text-4xl text-lite bg-[rgba(0,0,0,0.7)]">
+          <div className="w-9/12 h-full overflow-y-auto type-ui text-gray bg-black p-5">
+            <nav className="flex flex-col">
+              {(posts.children &&
+                Object.keys(posts.children).length !== 0 &&
+                Object.entries(posts.children).map(([k, v], i) => {
+                  return (
+                    <NavSection
+                      posts={v}
+                      root={join(root, k)}
+                      path={firstCrumb}
+                      key={"tray-" + join(root, k)}
+                      divider={i > 0}
+                      longLabels
+                    />
+                  );
+                })) || (
+                <NavSection
+                  posts={posts}
+                  root={root}
+                  path={firstCrumb}
+                  longLabels
+                />
+              )}
+            </nav>
+          </div>
+        </div>
+      )}
+      <button
+        className={classnames(
+          "flex md:hidden fixed bottom-4 right-4 items-center justify-center leading-none w-12 h-12 text-4xl text-lite bg-tint rounded-full",
+          { "z-50": isOpen, "z-40": !isOpen }
+        )}
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        {isOpen ? (
+          <svg
+            width="16"
+            height="16"
+            viewBox="0 0 16 16"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M0.39382 13.7045C-0.131273 14.2296 -0.131274 15.081 0.39382 15.6061C0.918913 16.1312 1.77026 16.1312 2.29535 15.6061L7.99999 9.90142L13.7047 15.6061C14.2297 16.1312 15.0811 16.1312 15.6062 15.6061C16.1313 15.081 16.1313 14.2296 15.6062 13.7046L9.90152 7.99989L15.6061 2.29535C16.1312 1.77026 16.1312 0.918913 15.6061 0.39382C15.081 -0.131273 14.2296 -0.131273 13.7045 0.39382L7.99999 6.09836L2.29548 0.393844C1.77038 -0.131249 0.919038 -0.13125 0.393945 0.393844C-0.131148 0.918937 -0.131148 1.77028 0.393945 2.29537L6.09846 7.99989L0.39382 13.7045Z"
+              className="fill-white"
+            />
+          </svg>
+        ) : (
+          <svg
+            width="16"
+            height="16"
+            viewBox="0 0 16 17"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <rect width="16" height="3" rx="1.5" className="fill-white" />
+            <rect y="7" width="16" height="3" rx="1.5" className="fill-white" />
+            <rect
+              y="14"
+              width="16"
+              height="3"
+              rx="1.5"
+              className="fill-white"
+            />
+          </svg>
+        )}
+      </button>
       <SideBar className="hidden md:flex">
         <nav className="flex flex-col flex-1 type-ui offset-r whitespace-nowrap overflow-y-auto overflow-x-hidden">
           {(posts.children &&

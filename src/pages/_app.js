796e652cce2492cb566190afc72fb5d7e6595d4e
ripperi <22086553+ripperi@users.noreply.github.com>
2023-10-28 15:31:20 +0100

    feat: favicon

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,24 +5,7 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <link rel="shortcut icon" href="/images/favicon.ico" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/images/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/images/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/images/favicon-16x16.png"
-        />
+        <link rel="shortcut icon" type="image/png" href="/images/favicon.png" />
       </Head>
       <Component {...pageProps} />
     </>

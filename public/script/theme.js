429bf72f209f6d054080a91a6b4f0a781412adcc
ripperi <22086553+ripperi@users.noreply.github.com>
2023-11-17 14:31:54 +0200

    feat: test new light mode palette

--- public/script/theme.js
+++ public/script/theme.js
@@ -1,7 +1,9 @@
-if (
-  window.matchMedia("(prefers-color-scheme: dark)").matches
-) {
-  document.documentElement.classList.add("dark");
-} else {
-  document.documentElement.classList.remove("dark");
-}
+// if (
+//   window.matchMedia("(prefers-color-scheme: dark)").matches
+// ) {
+//   document.documentElement.classList.add("dark");
+// } else {
+//   document.documentElement.classList.remove("dark");
+// }
+
+document.documentElement.classList.add("dark");
